046ee83c49b37860d3ecfcc8ae0dd4b4b402823c
lottverw <lotte.verwerft@student.arteveldehs.be>
2017-03-20 15:44:32 +0100

    Added reveal stories. (#4)
    * Added reveal stories.
    
    * Added reveal stories.
    
    * [WIP]
    
    * [WIP]

--- _stories/1617__gmb-story.md
+++ _stories/1617__gmb-story.md
@@ -2,12 +2,151 @@
 title: GMB story
 slug: gmb-story
 description: "Coming soon!"
+branding: "GMB"
 thumbnail:
     url: "gmb-story/thumb.jpg"
     alt: "Het GMB verhaal"
 blurred:
     url: "gmb-story/thumb-blur.jpg"
     alt: "Het GMB Verhaal"
+intro: 
+  - title: "de GMB case"
+    mobile:
+      text: "Deze website bevat audio en video. Een optimale gebruikerservaring verkrijg je met een snelle internetverbinding. Je kan zowel muiswiel als pijltjestoetsen gebruiken om te navigeren door het verhaal." 
+    desktop: 
+      text: "Deze website bevat audio en video. Een optimale gebruikerservaring verkrijg je met een snelle internetverbinding. Je kan zowel muiswiel als pijltjestoetsen gebruiken om te navigeren door het verhaal."
+sections:
+  - thumb: "slide01.png"
+    text:
+      position: "left"
+      title: "Grafimediabeleid helpt sites te promoten"
+      titlecolor: "white"
+    background:
+      type: "image"
+      url: "slide01.png"
+      mobile: "dark"
+  - thumb: "slide02.png"
+    text:
+      position: "left"
+      title: "Wat is het doel van de google online marketing challenge?"
+      titlehighlight: "CASE STUDY"
+      highlight: "green"
+      titlecolor: "blue"
+      textcolor: "dark"
+      paragraph: 
+      - line: "Er wordt een Adwords reclamebudget van $250 voorzien door Google. Hiermee bedenken en lanceren de GMBers een online reclamecam- pagne voor een non-profit organisatie. De campagne loopt over een periode van drie weken. Grafimediabeleidatwork."
+    background:
+      type: "image"
+      url: "slide02.png"
+      mobile: "light"
+  - thumb: "slide03.png"
+    text:
+      position: "left"
+      title: "Stappenplan"
+      titlecolor: "pink"
+      paragraph:
+      - line: "We werken aan de hand van een stappenplan. Zo kunnen we de situatie zo goed mogelijk analyseren en tot het gepaste resultaat komen."
+    background:
+      type: "image"
+      url: "slide03.png"
+      mobile: "light"
+  - thumb: "slide04.png"
+    text:
+      title: "Non-profit organisatie kiezen"
+      titlehighlight: "Stap 1"
+      highlight: "white"
+      position: "right"
+      title: "Eerste jaar"
+      titlecolor: "pink"
+      paragraph: 
+      - line: "Kies een VZW en bekijk hun website"
+      - line: "Ga langs en bespreek wat een online campagne voor hen kan betekenen."
+      - line: "Bepaal samen met hen de doelstellingen van de campagne"
+      - line: "Analyseer hun huidige website en ga op zoek naar de juiste trefwoorden"
+    background:
+      type: "image"
+      url: "slide04.png"
+      mobile: "light"
+      gradient: "gradient right"
+    stickers:
+      - url: "dreamcatchers.png"
+        position: "left bottom"
+        size: "large"
+  - thumb: "slide05.png"
+    text:  
+      title: "Non-profit organisatie kiezen"
+      titlehighlight: "Stap 2"
+      highlight: "white"
+      position: "left"
+      title: "Precampaign report opstellen"
+      titlecolor: "pink"
+      paragraph: 
+      - line: "Bespreek de organisatie en hun doelgroep."
+      - line: "Doe een marktanalyse"
+      - line: "Bepaal de AdWords strategie Stuur het rapport naar Google om toegang te krijgen tot je AdWords Competition Account"
+      - line: "Je kan nu via Google Adwords je advertenties aanmaken en beheren"
+    background:
+      type: "image"
+      url: "slide05.png"
+      mobile: "dark"
+      gradient: "gradient left"
+  - thumb: "slide06.png"
+    text:
+      title: "Non-profit organisatie kiezen"
+      titlehighlight: "Stap 3"
+      highlight: "white"
+      position: "right"
+      title: "Postcampaign report"
+      titlecolor: "pink"
+      paragraph: 
+      - line: "Schrijf de resultaten en conclusie van de campagne neer in een rapport."
+      - line: "Noteer aanbevelingen voor de vzw met oog op hun toekomstige online campagnes."
+    background:
+      type: "image"
+      url: "slide06.png"
+      mobile: "light"
+      gradient: "gradient right"
+  - thumb: "slide07.png"
+    text:
+      position: "left"
+      title: "Non-profit organisatie kiezen"
+      titlecolor: "blue"
+      titlehighlight: "Stap 3"
+      highlight: "white"
+      paragraph: 
+      - line: "Schrijf de resultaten en conclusie van de campagne neer in een rapport. Noteer aanbevelingen voor de vzw met oog op hun toekomstige online campagnes."
+    background:
+      type: "image"
+      url: "slide07.png"
+      mobile: "light"
+      gradient: "left gradient"
+  - thumb: "slide08.png"
+    text:
+      position: "right"
+      paragraph: 
+      - line: "Create"
+      - line: "Original"
+      - line: "Motivated and"
+      - line: "Progressive in"
+      - line: "Achieving a"
+      - line: "Solution with"
+      - line: "Spirit"
+    background:
+      type: "image"
+      url: "slide08.png"
+      mobile: "light"
+  - thumb: "slide09.png"
+    text: 
+      position: "left"
+      title: "AND THAT'S HOW WE WORK"
+      titlecolor: "blue"
+      paragraph: 
+      - line: "Wil je meer zien van onze studenten? Bekijk dan zeker eens de GDM portfolio of breng een bezoekje tijdens een van onze infodagen."
+    background:
+      type: "image"
+      url: "slide09.png"
+      mobile: "light"
+    contact: true
 created: 20/01/2017
 active: true
 enabled: false
---
+++ js/story.js
@@ -0,0 +1,244 @@
+(function () {
+    App = {
+        "init": function () {
+            App.nav.init();
+            App.intro.init();
+            App.text.init();
+            App.fullscreen.init();
+        },
+        "nav": {
+            "state": false,
+            "init": function () {
+                if (document.querySelector('.menu')) {
+                    App.nav.bindEventListeners();
+                }
+            },
+            "bindEventListeners": function () {
+                var menuItems = document.querySelector('.menu-items');
+                var items = menuItems.querySelectorAll('.nav-item > a');
+                var gridIcon = document.querySelector('.overview-toggle');
+                var menuIcon = document.querySelector('.menu-toggle');
+                var shareIcon = document.querySelector('.share-toggle');
+            
+                document.addEventListener('slidechanged', function (event) {
+                    App.nav.activeItem();
+                });
+
+                shareIcon.addEventListener("click", function () {
+                    App.nav.share();
+                });
+
+                menuIcon.addEventListener("click", function () {
+                    App.nav.toggleNavigation();
+                }, false);
+
+                menuItems.addEventListener("click", function () {
+                    App.nav.closeMenuOnClick();
+                }, false);
+
+                gridIcon.addEventListener("mouseover", function () {
+                    gridIcon.classList.remove("offhover");
+                    gridIcon.classList.add("hover");
+                    gridIcon.classList.add("link");
+                });
+                gridIcon.addEventListener("mouseleave", function () {
+                    gridIcon.classList.remove("hover");
+                    gridIcon.classList.remove("link");
+                    gridIcon.classList.add("offhover");
+                });
+                
+                for (i = 0; i <= items.length; i++) {
+                    if (items[i] != undefined){
+                        items[i].setAttribute("href", "#/" + i);
+                        items[i].classList.add(i);
+                    }
+                }
+            },
+            "share": function () {
+                var list = document.querySelector('.share-list');
+                if (list.classList.contains("inactive")) {
+                    list.classList.remove("inactive");
+                } else {
+                    list.classList.add("inactive");
+                }
+            },
+            "toggleNavigation": function () {
+                var icon = document.querySelector('.menu-toggle');
+                var menu = document.querySelector('.menu');
+
+                if (App.nav.state) {
+                    menu.classList.add("hide");
+                    menu.classList.remove("open");
+                    icon.classList.remove("close");
+                    icon.classList.add("open");
+                    App.nav.state = false;
+                } else {
+                    menu.classList.add("open");
+                    menu.classList.remove("hide");
+                    icon.classList.add("close");
+                    App.nav.state = true;
+                }
+            },
+            "activeItem": function () {
+                var state = Reveal.getState().indexh;
+                var container = document.querySelectorAll('.nav-item > a');
+                var items = document.querySelectorAll('.nav-item > a > img');
+                for (var i = 0; i <= items.length; i++) {
+                    if (items[i] != undefined && container[i].classList.contains(state)) {
+                        items[i].classList.add('active');
+                        
+                    }
+                    else if (items[i] != undefined) {
+                        items[i].classList.remove('active');
+                    }
+                }
+            },
+            "closeMenuOnClick": function () {
+                var icon = document.querySelector('.menu-toggle');
+                var menu = document.querySelector('.menu');
+
+                if (App.nav.state) {
+                    menu.classList.add("hide");
+                    menu.classList.remove("open");
+                    icon.classList.remove("close");
+                    icon.classList.add("open");
+                    App.nav.state = false;
+                } else {
+                    menu.classList.add("open");
+                    menu.classList.remove("hide");
+                    icon.classList.add("close");
+                    App.nav.state = true;
+                }
+            }
+        },
+        "intro": {
+            "state": false,
+            "init": function () {
+                if (document.querySelector('.intro')) {
+                    App.intro.bindEventListeners();
+                }
+            },
+            "bindEventListeners": function () {
+                var menu = document.querySelector('.menu');
+                var intro = document.querySelector('.intro');
+                var button = document.querySelector('.button');
+
+                if(!intro.classList.contains("hide")){
+                    App.text.makeTextSticky();
+                }
+                // DESKTOP CLOSE
+                button.addEventListener("click", function () {
+                    intro.classList.add('hide');
+                }, false);
+
+                // MOBILE CLOSE
+                intro.addEventListener("touchstart", function () {
+                    intro.classList.add('hide');
+                }, false)
+            }
+        },
+        "fullscreen": {
+            "state": false,
+            "init": function () {
+                if (document.querySelector('.menu')) {
+                    App.fullscreen.bindEventListeners();
+                }
+            },
+            "bindEventListeners": function () {
+                var icon = document.querySelector('.fullscreen');
+                icon.addEventListener('click', function() {
+                    App.fullscreen.enterFullscreen();
+                }, false);
+            },
+            "enterFullscreen": function () {
+                var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+                    (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+                    (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+                    (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+                var docElm = document.documentElement;
+                if (!isInFullScreen) {
+                    var video = document.querySelector('.present > video');
+
+                    var srcImage = document.querySelector('.fullscreen');
+                    srcImage.src = "../assets/img/icons/exitFullscreenButton.svg";
+
+                    var pointer = document.querySelector('.present > svg');
+                    if (pointer != undefined) {
+                        pointer.classList.add('hide-on-desktop');
+                    }
+                    if (docElm.requestFullscreen) {
+                        docElm.requestFullscreen();
+                    } else if (docElm.mozRequestFullScreen) {
+                        docElm.mozRequestFullScreen();
+                    } else if (docElm.webkitRequestFullScreen) {
+                        docElm.webkitRequestFullScreen();
+                    } else if (docElm.msRequestFullscreen) {
+                        docElm.msRequestFullscreen();
+                    }
+                } else {
+                    var srcImage = document.querySelector('.fullscreen');
+                    srcImage.src = "../assets/img/icons/fullscreenButton.svg";
+                    var pointer = document.querySelector('.present > svg');
+                    if (pointer != undefined) {
+                        pointer.classList.remove('hide-on-desktop');
+                    }
+                    if (document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if (document.webkitExitFullscreen) {
+                        document.webkitExitFullscreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.msExitFullscreen) {
+                        document.msExitFullscreen();
+                    }
+                }
+            }
+        },
+        "text": {
+            "state": false,
+            "init": function () {
+                if (document.querySelector('.text')) {
+                    App.text.bindEventListeners();
+                }
+            },
+            "bindEventListeners": function () {
+                var text = document.querySelector('section.present > mobile-light-text-bg');
+                if (text != undefined) {
+                    text.addEventListener('touchmove', function () {
+                        App.text.getSrollPersentage();
+                    }, false);
+                }
+
+                document.addEventListener('slidechanged', function () {
+                    var text = document.querySelector('.present > .text');
+                    var textlong = document.querySelector('.present > .text-long');
+
+                    if (text != undefined && text.classList.contains('stay') == false) {
+                          App.text.makeTextSticky();
+                    }  else if (textlong != undefined && textlong.classList.contains('stay') == false){
+                        App.text.makeTextSticky();
+                    }
+                });
+            },
+            "makeTextSticky": function () {
+                setTimeout(function () {
+                    var text = document.querySelector('section.present > .text');
+                    var textlong = document.querySelector('section.present > .text-long');
+                    if (textlong != null && !textlong.classList.contains('stay')) {
+                        textlong.classList.add("stay");
+                        textlong.classList.remove("text-fade-in");
+                    } else if (text != null && !text.classList.contains('stay')){
+                        text.classList.remove("text-fade-in");
+                        text.classList.add("stay");
+                    }
+                }, 3000);
+            },
+        }
+    };
+
+    App.init();
+
+})();
+
+
